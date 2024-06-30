19a0227b47e27007e123b3f1dfe327a479ba06b8
Abishek-GH <abishek0149@gmail.com>
2024-07-01 00:44:52 +0530

    Final Commit

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ app.set("views", path.join(__dirname, "views"));
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
-const bookingRouter = require("./routes/bookingRoutes");
 
 // Getting the enivornment variables
 // console.log(app.get("env"));
@@ -126,7 +125,6 @@ app.use("/", viewRouter);
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
-app.use("/api/v1/bookings", bookingRouter);
 
 app.all('*', (req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
--- routes/bookingRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const bookingController = require("./../controllers/bookingController");
-const authController = require("./../controllers/authController");
-
-
-
-module.exports = router;
