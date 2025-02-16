949c288a6788ae53a1e71fda5a0dc4e7ee76f400
Abishek-GH <abishek0149@gmail.com>
2025-02-17 00:33:59 +0530

    Add My Bookings Page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,7 @@ const authController = require('./../controllers/authController');
 const router = express.Router();
 
 router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get('/my-tours', authController.protect, viewsController.getMyTours);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
